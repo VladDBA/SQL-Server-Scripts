45569e81b796b238ac6efeac591b0c3f7cdfc32c
Vlad Drumea <48413726+VladDBA@users.noreply.github.com>
2023-09-21 19:40:26 +0300

    Update MassUpdateDatabaseOwnership.sql

--- Scripts/MassUpdateDatabaseOwnership.sql
+++ Scripts/MassUpdateDatabaseOwnership.sql
@@ -7,24 +7,27 @@
 USE [master]
 GO
 
-		/* Variable declaration */
+/* Variable declaration */
 DECLARE @ExecSQL      NVARCHAR(1000),
         @OldOwner     NVARCHAR(128),
         @NewOwner     NVARCHAR(128),
         @DatabaseName NVARCHAR(128);
 
-
-SET @OldOwner = N'OldUserName'; /*Change this*/
+SET @OldOwner = N'any'; /*Change this to either a specific login name or use 
+						any to target all databases where current owner <> @NewOwner
+						*/
 SET @NewOwner = N'sa'; /*Change this if needed*/
-
 
 DECLARE ChangeDBOwner CURSOR LOCAL STATIC READ_ONLY FORWARD_ONLY FOR
   SELECT DISTINCT N'[' + [db].[name] + N']'
   FROM   [master].[sys].[databases] [db]
          LEFT JOIN [master].[sys].[server_principals] AS [sp]
                 ON [db].[owner_sid] = [sp].[sid]
-  WHERE
-    [sp].[name] = @OldOwner;
+  WHERE  [sp].[name] = CASE
+                         WHEN LOWER(@OldOwner) = N'any' THEN [sp].[name]
+                         ELSE @OldOwner
+                       END
+         AND [sp].[name] <> @NewOwner;
 
 OPEN ChangeDBOwner;
 
@@ -32,14 +35,15 @@ FETCH NEXT FROM ChangeDBOwner INTO @DatabaseName;
 
 WHILE @@FETCH_STATUS = 0
   BEGIN
-      SET @ExecSQL = N'ALTER AUTHORIZATION ON DATABASE::' + @DatabaseName +
-                     N' TO ['
-                     + @NewOwner + N'];';
+      SET @ExecSQL = N'ALTER AUTHORIZATION ON DATABASE::'
+                     + @DatabaseName + N' TO [' + @NewOwner + N'];';
 
-      EXEC (@ExecSQL);
+      PRINT N'Changing ownership of '+@DatabaseName + N' to '+ @NewOwner
+	  EXEC (@ExecSQL);
 
       FETCH NEXT FROM ChangeDBOwner INTO @DatabaseName;
   END;
 
 CLOSE ChangeDBOwner;
+
 DEALLOCATE ChangeDBOwner;
